8ea9cd75f0041a0c04030e5c58d2f204eaa129ab
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-22 21:44:55 +0600

    Delete example files from project

--- src/api/exampleAPI/controllers/exampleController.js
+++
@@ -1,8 +0,0 @@
-// example api controller file
-
-const exampleController = async (req, res) => {
-  const requestBody = req.body;
-  res.send("controller working");
-};
-
-module.exports = exampleController;
