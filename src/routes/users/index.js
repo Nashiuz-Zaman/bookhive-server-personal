963ddc3b56dc3a16a2513609626365b18c26a35f
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-09 18:51:57 +0600

    Add both apis in their respective routes in the index file

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,17 +1,23 @@
 // imports
 const express = require("express");
+
+// api controllers
 const createUser = require("../../api/users/controllers/createUser");
 const updateUser = require("../../api/users/controllers/updateUser");
 const checkUser = require("../../api/users/controllers/checkUser");
 const getMembers = require("../../api/users/controllers/getMembers");
+const getUsers = require("../../api/users/controllers/getUsers");
+const deleteUser = require("../../api/users/controllers/deleteUser");
 
 // create router
 const router = express.Router();
 
 // routes
+router.get("/users", getUsers);
 router.post("/users", createUser);
 router.post("/users/check", checkUser);
 router.patch("/users/:email", updateUser);
 router.get("/users/members", getMembers);
+router.delete("/users/:id", deleteUser);
 
 module.exports = router;
